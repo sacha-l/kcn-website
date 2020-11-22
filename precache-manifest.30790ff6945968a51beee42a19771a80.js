b959f7ba03f50ddf98a1973a79100ab6ff0be7f1
sacha-l <alansky@localhost.localdomain>
2020-11-22 09:16:32 -0500

    Updates

---
+++ precache-manifest.30790ff6945968a51beee42a19771a80.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a48732ce54dd0e15b897f723384a26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef97f2a5f5bc5773da42",
+    "url": "/static/css/main.e08bb8ca.chunk.css"
+  },
+  {
+    "revision": "5cbb27f197a31bf3ae1b",
+    "url": "/static/js/2.4477f574.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.4477f574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef97f2a5f5bc5773da42",
+    "url": "/static/js/main.5cd158f6.chunk.js"
+  },
+  {
+    "revision": "50fc89820ffe49eeb598",
+    "url": "/static/js/runtime-main.b63f9581.js"
+  },
+  {
+    "revision": "747d67ed39853b49a89f667cf1cea6bb",
+    "url": "/static/media/Bitswift_Grid-Horizontal-Alt-1.747d67ed.png"
+  },
+  {
+    "revision": "5fb88c6306a27b7a1e5f1d305e475f52",
+    "url": "/static/media/Bitswift_Grid-Verticle-1.5fb88c63.png"
+  },
+  {
+    "revision": "c1ecd6309b85a1cdd325718f1ff394d4",
+    "url": "/static/media/bar-logo.c1ecd630.png"
+  },
+  {
+    "revision": "c51704204494043f41f640e5f6911fe5",
+    "url": "/static/media/bitswift-canadian.c5170420.png"
+  },
+  {
+    "revision": "fd64aec7fa4f73bb1d4ab351d27ae45f",
+    "url": "/static/media/bitswift-merchants.fd64aec7.png"
+  },
+  {
+    "revision": "ad33aded702db52297c412c87d8d7eda",
+    "url": "/static/media/clothing-logo.ad33aded.png"
+  }
+]);
